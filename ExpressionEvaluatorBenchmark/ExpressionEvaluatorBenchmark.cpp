e5203cc2b1041bc323547b8256485abdc03ded85
Alexander <a1ex.chaos.exe@gmail.com>
2016-05-01 14:48:42 +0500

    Add benchmarks

---
+++ ExpressionEvaluatorBenchmark/ExpressionEvaluatorBenchmark.cpp
@@ -0,0 +1,72 @@
+#include "../ExpressionEvaluatorLib/ExpressionEvaluatorLib.h"
+#include <iostream>
+#include <functional>
+#include <omp.h>
+
+double sumInInterval(double from, double to, size_t steps, std::function<double(double)> func)
+{
+	double value = from;
+	double step = (to - from) / steps;
+
+	double sum = 0;
+
+	while (value <= to)
+	{
+		sum += func(value);
+		value += step;
+	}
+
+	return sum;
+}
+
+double avgTime(std::function<void()> func, size_t repeat)
+{
+	double start = omp_get_wtime();
+
+	for (size_t i = 0; i < repeat; ++i)
+	{
+		func();
+	}
+
+	double end = omp_get_wtime();
+
+	return ((end - start) / repeat) * 1000;
+}
+
+int main()
+{
+	Expression expr("(cos(x)^0.5*cos(200*x)+abs(x)^0.5-0.7)*(4-x^2)^0.01");
+
+	const double from = -1.6;
+	const double to = 1.6;
+	const size_t steps = 1000000;
+
+	const size_t bench_repeats = 15;
+
+	double exprTime = avgTime([&]
+	{
+		sumInInterval(from, to, steps, [&expr](double value)
+		{
+			expr.SetParameter("x", value);
+			return expr.Execute();
+		});
+	}, bench_repeats);
+
+	std::cout << "Expression: " << exprTime << "ms" << std::endl;
+
+	double nativeTime = avgTime([&from, &to, &steps]
+	{
+		sumInInterval(from, to, steps, [](double value)
+		{
+			return (pow(cos(value), 0.5) * cos(200 * value) + pow(abs(value), 0.5) - 0.7) * (4 - pow(pow(value, 2), 0.01));
+		});
+	}, bench_repeats);
+
+	double percantage = (exprTime / nativeTime * 100.0) - 100;
+
+	std::cout << "Native: " << nativeTime << "ms"
+		<< " (" << abs(percantage) << "% " << (percantage > 0 ? "faster" : "slower") << ")"
+		<< std::endl << std::endl;
+
+	return 0;
+}
---
+++ ExpressionEvaluatorNetBenchmark/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+using ExpressionEvaluatorNet;
+
+namespace ExpressionEvaluatorNetBenchmark
+{
+    public class FeelVsManaged
+    {
+        private const double From = -1.6;
+        private const double To = 1.6;
+        private const uint Steps = 1000000;
+
+        private double Sum(Func<double, double> func)
+        {
+            var value = From;
+            var step = (To - From) / Steps;
+
+            double sum = 0;
+
+            while(value <= To)
+            {
+                sum += func(value);
+                value += step;
+            }
+
+            return sum;
+        }
+
+        public double ManagedTest()
+        {
+            return Sum(value => (Math.Pow(Math.Cos(value), 0.5) * Math.Cos(200 * value) + Math.Pow(Math.Abs(value), 0.5) - 0.7) * (4 - Math.Pow(Math.Pow(value, 2), 0.01)));
+        }
+
+        public double FeelTest()
+        {
+            var expr = new ExpressionEvaluator("(cos(x)^0.5*cos(200*x)+abs(x)^0.5-0.7)*(4-x^2)^0.01");
+            return Sum(value =>
+            {
+                expr.SetVariableValue("x", value);
+                return expr.Execute();
+            });
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var bench = new FeelVsManaged();
+            const uint benchmarkRepeats = 15;
+
+            for(uint i = 0; i < benchmarkRepeats; i++)
+            {
+                bench.FeelTest();
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for(uint i = 0; i < benchmarkRepeats; i++)
+            {
+                bench.FeelTest();
+            }
+
+            stopWatch.Stop();
+
+            var expressionTime = stopWatch.Elapsed.TotalMilliseconds / benchmarkRepeats;
+
+            Console.WriteLine("Expression: {0:F} ms", expressionTime);
+
+            for(uint i = 0; i < benchmarkRepeats; i++)
+            {
+                bench.ManagedTest();
+            }
+
+            stopWatch = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for(uint i = 0; i < benchmarkRepeats; i++)
+            {
+                bench.ManagedTest();
+            }
+
+            stopWatch.Stop();
+
+            var managedTime = stopWatch.Elapsed.TotalMilliseconds / benchmarkRepeats;
+
+            var percentage = expressionTime / managedTime - 1;
+
+            Console.WriteLine("Managed: {0:F} ms ({1:p} {2})", managedTime, Math.Abs(percentage), percentage > 0 ? "faster" : "slower");
+        }
+    }
+}
